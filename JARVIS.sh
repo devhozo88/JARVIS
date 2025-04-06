e3363d4a54b62c01476645592ad0489cd62c41e1
devhozo88 <hozoowarnet@gmail.com>
2025-04-06 12:50:33 +0700

    Update JARVIS.sh

--- JARVIS.sh
+++ JARVIS.sh
@@ -11,10 +11,18 @@ echo "=================================="
 read -p "Masukkan pilihan Anda [1-2]: " pilihan
 
 if [ "$pilihan" -eq 1 ]; then
-    echo "Memutar hozo.mp3..."
-    mpv hozo.mp3
+    echo "Memutar hozoo.mp3..."
+    mpv --no-terminal hozo.mp3
+    clear
+    git clone https://github.com/Kingcy78/FLIPPERZERO
+
+cd FLIPPERZERO
+
+bash FLIPPERZERO-INDONESIA404.sh
 elif [ "$pilihan" -eq 2 ]; then
+mpv --no-terminal bye.mp3
     echo "Keluar... Terima kasih!"
+    
     exit 0
 else
     echo "Pilihan tidak valid!"
